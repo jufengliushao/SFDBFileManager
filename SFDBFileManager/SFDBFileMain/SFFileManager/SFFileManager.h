93e165e9c3385be0c59228f18c529f78667f5d55
刘少锋 <liushaofeng@cnlive.com>
2017-03-16 09:01:53 +0800

    create files

---
+++ SFDBFileManager/SFDBFileMain/SFFileManager/SFFileManager.h
@@ -0,0 +1,13 @@
+//
+//  SFFileManager.h
+//  SFDBFileManager
+//
+//  Created by cnlive-lsf on 2017/3/16.
+//  Copyright © 2017年 lsf. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SFFileManager : NSObject
+
+@end
---
+++ SFDBFileManager/SFDBFileMain/SFFileManager/SFFileManager.m
@@ -0,0 +1,13 @@
+//
+//  SFFileManager.m
+//  SFDBFileManager
+//
+//  Created by cnlive-lsf on 2017/3/16.
+//  Copyright © 2017年 lsf. All rights reserved.
+//
+
+#import "SFFileManager.h"
+
+@implementation SFFileManager
+
+@end
