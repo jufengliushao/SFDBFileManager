90aa7b5ae227d5c359bd35e5099f2b0a21e071e6
刘少锋 <liushaofeng@cnlive.com>
2017-03-16 09:00:00 +0800

    create project

---
+++ SFDBFileManager/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SFDBFileManager
+//
+//  Created by cnlive-lsf on 2017/3/16.
+//  Copyright © 2017年 lsf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
