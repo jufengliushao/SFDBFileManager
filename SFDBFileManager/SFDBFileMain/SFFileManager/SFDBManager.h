014cd995e73adb7e1a3af5efa37ec993fa3a37b9
liushaofeng <jufengliushao@163.com>
2017-04-11 17:03:09 +0800

    create DBManager

---
+++ SFDBFileManager/SFDBFileMain/SFFileManager/SFDBManager.h
@@ -0,0 +1,13 @@
+//
+//  SFDBManager.h
+//  SFDBFileManager
+//
+//  Created by cnlive-lsf on 2017/4/11.
+//  Copyright © 2017年 lsf. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SFDBManager : NSObject
+
+@end
---
+++ SFDBFileManager/SFDBFileMain/SFFileManager/SFDBManager.m
@@ -0,0 +1,13 @@
+//
+//  SFDBManager.m
+//  SFDBFileManager
+//
+//  Created by cnlive-lsf on 2017/4/11.
+//  Copyright © 2017年 lsf. All rights reserved.
+//
+
+#import "SFDBManager.h"
+
+@implementation SFDBManager
+
+@end
